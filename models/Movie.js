efefce95174aee452b85a3c95ed5d63532a38909
Eren Can Şahin <eren.sahin10@ogr.sakarya.edu.tr>
2024-07-21 18:44:21 +0300

    routers and validations done

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true
+        required: [true,"'{PATH}' alanı zorunludur."],
+        maxlength: [30, "'{PATH}' alanı ('{VALUE}'), {MAXLENGTH} karakterden küçük olmalıdır."],
+        minlength: [3, "'{PATH}' alanı ('{VALUE}'), {MINLENGTH}'den büyük olmalıdır."]
     },
-    category: String,
-    county: String,
-    year: Number,
-    imdb_score:Number,
+    category: {
+        type:String,
+        maxlength:30,
+        minlength:1
+    },
+    county: {
+        type:String,
+        maxlength:30,
+        minlength:1
+    },
+    year: {
+        type:Number,
+        max:2040,
+        min:1900
+    },
+    imdb_score:{
+        type:Number,
+        max:10,
+        min:0,
+    },
     date:{
         type:Date,
         default:Date.now
